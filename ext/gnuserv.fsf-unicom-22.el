20e9fcdf82fef41ecc85cf41addf2b18ad96d0fd
lieutar <lieutar@1dk.jp>
2010-11-24 09:52:28 +0900

    merged!

---
+++ ext/gnuserv.fsf-unicom-22.el
@@ -0,0 +1,4 @@
+
+(require 'gnuserv-compat)
+(gnuserv-start)
+
---
+++ ext/zz-custom.fsf-unicom-22.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(custom-file "~/.emacs.d/rc/ext/custom.fsf-22.el"))
+
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(org-level-1 ((((class color) (min-colors 88)
+                  (background light))
+                 (:foreground "#666"
+                              :inverse-video t :overline t :underline t :weight ultra-bold :height 2.0 :width condensed)))))
+
+
