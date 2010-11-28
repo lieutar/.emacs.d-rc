179352f8e61148b549599c2d7b5eebb85b6b1e2b
lieutar <lieutar@1dk.jp>
2010-11-28 18:46:31 +0900

    acman の読み込みを ext/00-acman.el に移した。また、twittering に xauth の設定をした。

--- base/account.el
+++
@@ -1,9 +0,0 @@
-(require 'acman)
-
-(let ((mail (acman-get-property "*about*" "*me*" "email")))
-  (setq g-user-email      mail)
-  (setq gcal-user-email     mail)
-  (setq gblogger-user-email mail)
-  (setq kael-hatena-user-alist
-        (acman-get-property-alist "hatena.ne.jp" "password")))
-
